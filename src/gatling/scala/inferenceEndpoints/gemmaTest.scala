555d4fa600d95d76d50941739bee2c9214a5f069
earlalvarado-pi <earl.alvarado@pureinsights.com>
2024-03-22 16:30:28 -0600

    Gemma 10100

---
+++ src/gatling/scala/inferenceEndpoints/gemmaTest.scala
@@ -0,0 +1,35 @@
+package inferenceEndpoints
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class publicEndpointTest extends Simulation {
+
+  val feeder = csv("search.csv").random
+  val questionsFeeder = csv("questions.csv").random
+  val defaultEndpoint= ""
+  val endpoint = System.getProperty("endpointUrl", defaultEndpoint)
+
+  val postQuestions= scenario("Post JSON Data")
+    .feed(questionsFeeder)
+    .exec(
+      http("PostRequest")
+        .post(endpoint)
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        //.header("Authorization", "Bearer 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")
+        .body(StringBody("""{
+                           "inputs": {
+                             "context": "${Answer}",
+                             "question": "What is my name?"
+                           }
+                         }""")).asJson
+        .check(status.is(200))
+    )
+
+  val users = scenario("Users").exec(postQuestions)
+
+  setUp(
+    users.inject(rampUsers(10).during(100)),
+  )
+}
